799bc27c9e21b539e16236993ad1729952b55521
Carlopher Jay Sarmiento <sarmientocarlopher@gmail.com>
2021-07-07 10:48:26 -0400

    Finished the timer function and score

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,9 @@
-
 let mainContentEl = document.querySelector("#main-content");
 let questionsEl = document.querySelector("#questions");
 let choicesEl = document.querySelector("#choices");
+let answerEl = document.querySelector("#answer");
 let btnTimer = document.querySelector("#start-timer");
+let result = document.querySelector("#results");
 
 let startBtn = document.createElement("button");
 startBtn.innerHTML = "Start Quiz";
@@ -10,7 +11,6 @@ startBtn.type = "button";
 startBtn.name = "startBtn";
 startBtn.style.textAlign = "center";
 btnTimer.appendChild(startBtn);
-
 
 // Declaration of Questions
 const questionsObj = [
@@ -29,117 +29,153 @@ const questionsObj = [
     },
     answer: "3",
   },
-  {
-    question: "Arrays is Javascript can be used to store _____.",
-    choices: {
-      1: "numbers and strings",
-      2: "other arrays",
-      3: "booleans",
-      4: "all of the above",
-    },
-    answer: "4",
-  },
-  {
-    question: "String variables must be enclosed within _____ when being assigned to variables.",
-    choices: {
-      1: "commas",
-      2: "quotes",
-      3: "curly brackets",
-      4: "parentheses",
-    },
-    answer: "2",
-  },
+  // {
+  //   question: "Arrays is Javascript can be used to store _____.",
+  //   choices: {
+  //     1: "numbers and strings",
+  //     2: "other arrays",
+  //     3: "booleans",
+  //     4: "all of the above",
+  //   },
+  //   answer: "4",
+  // },
+  // {
+  //   question:
+  //     "String variables must be enclosed within _____ when being assigned to variables.",
+  //   choices: {
+  //     1: "commas",
+  //     2: "quotes",
+  //     3: "curly brackets",
+  //     4: "parentheses",
+  //   },
+  //   answer: "2",
+  // },
 ];
 
 const lastQuestion = questionsObj.length - 1;
 let questionsCtr = 0;
+let totalTime = 100;
 let score = 0;
+let deductPoint = 0;
 
 // This function starts the timer countdown
-var startTimer = function timer() {
+function startTimer() {
   let displayTimer = document.getElementById("timer");
-  const totalTime = 100;
-  let timeLeft = totalTime;
-  let timePassed = 0;
 
   const timer = setInterval(() => {
-    timeLeft = totalTime - timePassed;
-    timePassed += 1;
-    displayTimer.innerHTML = "Time Remaining: " + timeLeft;
-    if (timeLeft < 1) {
+    totalTime = totalTime - (1 + deductPoint);
+    deductPoint = 0;
+
+    timePassed = deductPoint + 1;
+    score = totalTime;
+    displayTimer.innerHTML = "Time Remaining: " + totalTime;
+
+    if (totalTime < 1) {
       clearInterval(timer);
       score = 0;
     }
   }, 1000);
-  score = timeLeft;
-};
-
-
+}
 
 // This function displays all the questions
 function displayQuestions() {
-  //hide Start button
-  startBtn.style.display = 'none';
+  //Element for holding the result of answer
 
-  
-  // variable to store the HTML output
+  //hide Start button
+  startBtn.style.display = "none";
+
+  // variable to store the Questions output
   const output = [];
 
-  //questionsObj.forEach((currQuestion, index) => {
-    // variable to store the list of choices
-    const choices = [];
+  // variable to store the list of choices
+  const choices = [];
 
-    let q = questionsObj[questionsCtr];
-  
-    for (choice in q.choices) {
-    
-      choices.push(
-        `<div>
+  let q = questionsObj[questionsCtr];
+
+  for (choice in q.choices) {
+    choices.push(
+      `<div>
           <button name="question${questionsCtr}" onclick='checkAnswer(${choice})'>${choice} ${q.choices[choice]}</button>           
         </div>
         `
-      );
-    }
-    // add this question and its answers to the output 
-    output.push(
-      `
+    );
+  }
+  // add this question and its answers to the output
+  output.push(
+    `
         <div class="questions"> ${q.question} </div>
         <div class="choices"> ${choices.join("")} </div>
    
-      `);
- // });
-  // finally combine our output list into one string of HTML and put it on the page
-  
-  mainContentEl.innerHTML = output.join("");
+      `
+  );
 
+  // finally combine our output list into one string of HTML and put it on the page
+  mainContentEl.innerHTML = output.join("");
 }
 
-function checkAnswer(answer){
-  console.log(lastQuestion);
-  if(answer === parseInt(questionsObj[questionsCtr].answer)){
-    alert('correct');
-  }else{
-    alert('wrong');
-    displayScore();
+function checkAnswer(answer) {
+  deductPoint = 0;
+  if (answer === parseInt(questionsObj[questionsCtr].answer)) {
+    displayResult("CORRECT");
+    // answerDisplay.textContent = "WRONG";
+  } else {
+    displayResult("WRONG");
+    deductPoint = 10;
   }
 
-  
-  if(questionsCtr <= lastQuestion){
+  if (questionsCtr < lastQuestion) {
     questionsCtr++;
     displayQuestions();
+  } else {
+    //clearInterval(startTimer);
+    showScore(score);
+    console.log("End of Code Quiz");
+
+    //displayFinalScore();
   }
- 
 }
 
-function displayScore(){
-  let result = document.getElementById('results');
-  result.innerHTML=`${score}`;
+function displayResult(result) {
+  //alert(result);
+  let answerDisplay = document.createElement("h4");
+  answerDisplay.textContent = result;
+  answerEl.appendChild(answerDisplay);
 }
 
+function showScore(score) {
+  questionsEl.style.display = "none";
+  choicesEl.style.display = "none";
+  startBtn.style.display = "block";
+  // let hideQ = document.getElementById("questions");
+  // hideQ.style.display = "none";
 
+  let scoreDiv = document.createElement("div");
+  let scoreH1El = document.createElement("h1");
+  scoreH1El.innerHTML = "All done!";
 
+  let scoreResult = document.createElement("p");
+  scoreResult.textContent = `Your final score is ${score}`;
 
+  let nameLabel = document.createElement("span");
+  nameLabel.textContent = "Enter your initials: ";
 
+  let nameInput = document.createElement("input");
+  nameInput.setAttribute("type", "text");
+
+  let submitScore = document.createElement("button");
+  submitScore.textContent = "Submit";
+
+  scoreDiv.appendChild(scoreH1El);
+  scoreDiv.appendChild(scoreResult);
+  scoreDiv.appendChild(nameLabel);
+  scoreDiv.appendChild(nameInput);
+  scoreDiv.appendChild(submitScore);
+  result.appendChild(scoreDiv);
+}
+
+// Function to save the score
+function saveScore() {}
+
+// Event Listeners
 startBtn.addEventListener("click", startTimer);
 startBtn.addEventListener("click", displayQuestions);
-
